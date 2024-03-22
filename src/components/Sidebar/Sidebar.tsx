f5db5d2fa599311e2ecb712a6f760e0f95c49212
nikachitava <chitava18.official@gmail.com>
2024-03-22 20:21:20 +0200

    add neccesery routes and pages

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -2,57 +2,60 @@ import { ReactNode, createContext, useState } from "react";
 import logo from "/images/icons/logo.svg";
 import { IoMdArrowRoundBack } from "react-icons/io";
 import { IoMdArrowRoundForward } from "react-icons/io";
+import { Link } from "react-router-dom";
 
 interface ISideBarProps {
-	children: ReactNode;
+    children: ReactNode;
 }
 
 interface SideBarContextProps {
-	expanded: boolean;
+    expanded: boolean;
 }
 
 export const SideBarContext = createContext<SideBarContextProps>({
-	expanded: true,
+    expanded: true,
 });
 
 export const SideBar: React.FC<ISideBarProps> = ({ children }) => {
-	const [expanded, setExpanded] = useState(true);
+    const [expanded, setExpanded] = useState(true);
 
-	const handleSideBar = () => {
-		setExpanded((curr) => !curr);
-	};
-	return (
-		<aside className={`h-screen`}>
-			<nav className="h-full flex flex-col bg-[#F9FAFB] border-r shadow-sm">
-				<div className="flex justify-between items-center px-8 py-6">
-					<img
-						src={logo}
-						className={`overflow-hidden transition-all ${
-							expanded ? "" : "w-0 h-0"
-						}`}
-						alt="logo"
-					/>
+    const handleSideBar = () => {
+        setExpanded((curr) => !curr);
+    };
+    return (
+        <aside className={`h-screen`}>
+            <nav className="h-full flex flex-col bg-[#F9FAFB] border-r shadow-sm">
+                <div className="flex justify-between items-center px-8 py-6">
+                    <Link to={"/"}>
+                        <img
+                            src={logo}
+                            className={`overflow-hidden transition-all ${
+                                expanded ? "" : "w-0 h-0"
+                            }`}
+                            alt="logo"
+                        />
+                    </Link>
 
-					{expanded ? (
-						<IoMdArrowRoundBack
-							size={20}
-							color="#919EAB"
-							className="cursor-pointer"
-							onClick={handleSideBar}
-						/>
-					) : (
-						<IoMdArrowRoundForward
-							size={20}
-							color="#919EAB"
-							className="cursor-pointer"
-							onClick={handleSideBar}
-						/>
-					)}
-				</div>
-				<SideBarContext.Provider value={{ expanded }}>
-					<ul className="flex-1 mt-10">{children}</ul>
-				</SideBarContext.Provider>
-			</nav>
-		</aside>
-	);
+                    {expanded ? (
+                        <IoMdArrowRoundBack
+                            size={20}
+                            color="#919EAB"
+                            className="cursor-pointer"
+                            onClick={handleSideBar}
+                        />
+                    ) : (
+                        <IoMdArrowRoundForward
+                            size={20}
+                            color="#919EAB"
+                            className="cursor-pointer"
+                            onClick={handleSideBar}
+                        />
+                    )}
+                </div>
+                <SideBarContext.Provider value={{ expanded }}>
+                    <ul className="flex-1 mt-10">{children}</ul>
+                </SideBarContext.Provider>
+            </nav>
+        </aside>
+    );
 };
--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -6,22 +6,41 @@ import { MdOutlineWhatshot, MdFeed } from "react-icons/md";
 import { SideBarItem } from "../components/SideBarItem/SideBarItem";
 import { NavBar } from "../components/NavBar/NavBar";
 import { Header } from "../components/Header/Header";
+import { Link } from "react-router-dom";
 
 export const Root = () => {
     return (
         <div className="grid grid-cols-[auto,1fr] grid-rows-[100vh] ">
             <SideBar>
-                <SideBarItem icon={<FaHome size={20} />} text="Home" active />
-                <SideBarItem
-                    icon={<FaRegCompass size={20} />}
-                    text="Discover"
-                />
-                <SideBarItem
-                    icon={<MdOutlineWhatshot size={20} />}
-                    text="Popular Products"
-                />
-                <SideBarItem icon={<MdFeed size={20} />} text="Feed" />
-                <SideBarItem icon={<FiPhoneCall size={20} />} text="Contact" />
+                <Link to={"/"}>
+                    <SideBarItem
+                        icon={<FaHome size={20} />}
+                        text="Home"
+                        active
+                    />
+                </Link>
+                <Link to={"/discover"}>
+                    <SideBarItem
+                        icon={<FaRegCompass size={20} />}
+                        text="Discover"
+                    />
+                </Link>
+                <Link to={"popular"}>
+                    <SideBarItem
+                        icon={<MdOutlineWhatshot size={20} />}
+                        text="Popular Products"
+                    />
+                </Link>
+
+                <Link to={"feed"}>
+                    <SideBarItem icon={<MdFeed size={20} />} text="Feed" />
+                </Link>
+                <Link to={"contact"}>
+                    <SideBarItem
+                        icon={<FiPhoneCall size={20} />}
+                        text="Contact"
+                    />
+                </Link>
             </SideBar>
             <div className="content grid-row-1/2 overflow-y-auto p-6">
                 <NavBar />
