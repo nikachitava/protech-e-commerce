b81bed86f02457eb3e9dd74f8083c69474429c80
nikachitava <chitava18.official@gmail.com>
2024-04-12 21:45:00 +0300

    get category items from mysql

--- src/components/Category/Category.tsx
+++ src/components/Category/Category.tsx
@@ -1,42 +1,38 @@
+import { useEffect, useState } from "react";
 import { CategoryItem } from "../CategoryItem/CategoryItem";
+import axios from "axios";
+
+interface Category {
+    categoryID: number;
+    categoryName: string;
+}
 
 export const Category = () => {
+    const [categories, setCategories] = useState<Category[]>([]);
+
+    useEffect(() => {
+        const fetchCategories = async () => {
+            try {
+                const response = await axios.get(
+                    "http://localhost:3000/categories"
+                );
+                setCategories(response.data);
+                console.log(response.data);
+            } catch (error) {
+                console.log("errror");
+            }
+        };
+        fetchCategories();
+    }, []);
+
     return (
         <div className="">
-            <ul className="w-[1140px] overflow-x-auto flex items-center gap-7 cursor-grab">
-                <li>
-                    <CategoryItem categoryName={"All"} />
-                </li>
-                <li>
-                    <CategoryItem categoryName={"Clothes"} />
-                </li>
-                <li>
-                    <CategoryItem categoryName={"Food"} />
-                </li>
-                <li>
-                    <CategoryItem categoryName={"Tech"} />
-                </li>
-                <li>
-                    <CategoryItem categoryName={"All"} />
-                </li>
-                <li>
-                    <CategoryItem categoryName={"All"} />
-                </li>
-                <li>
-                    <CategoryItem categoryName={"All"} />
-                </li>
-                <li>
-                    <CategoryItem categoryName={"All"} />
-                </li>
-                <li>
-                    <CategoryItem categoryName={"All"} />
-                </li>
-                <li>
-                    <CategoryItem categoryName={"asd"} />
-                </li>
-                <li>
-                    <CategoryItem categoryName={"Alqqweweqwewl"} />
-                </li>
+            <ul className="overflow-x-auto flex items-center gap-7 cursor-grab">
+                {categories.map((category) => (
+                    <li key={category.categoryID}>
+                        <CategoryItem categoryName={category.categoryName} />
+                    </li>
+                ))}
             </ul>
         </div>
     );
