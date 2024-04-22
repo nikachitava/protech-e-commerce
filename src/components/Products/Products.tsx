0ec12bed638c113d8bcfbc6711e7762031fca8b0
nikachitava <chitava18.official@gmail.com>
2024-04-22 17:49:17 +0300

    remove unneccesery attributs

--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -70,16 +70,11 @@ export const Products = () => {
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
                         viewBox="0 0 24 24"
-                        stroke-width="2"
                         stroke="currentColor"
                         aria-hidden="true"
                         className="w-4 h-4"
                     >
-                        <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
-                        ></path>
+                        <path d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"></path>
                     </svg>
                     Previous
                 </button>
@@ -95,16 +90,11 @@ export const Products = () => {
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
                         viewBox="0 0 24 24"
-                        stroke-width="2"
                         stroke="currentColor"
                         aria-hidden="true"
                         className="w-4 h-4"
                     >
-                        <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
-                        ></path>
+                        <path d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"></path>
                     </svg>
                 </button>
             </div>
