2504abaaf6fb7d3c5b4450ecfe4431347c7f6248
nikachitava <chitava18.official@gmail.com>
2024-05-06 18:22:38 +0300

    add balance in avatardropdownmenu

--- src/components/UserAvatarDropDown/UserAvatarDropDown.tsx
+++ src/components/UserAvatarDropDown/UserAvatarDropDown.tsx
@@ -30,7 +30,7 @@ export const UserAvatarDropDown: React.FC<IUserAvatar> = ({ avatar }) => {
         };
     }, []);
 
-    const { logout } = useContext(AuthContext);
+    const { logout, currentUser } = useContext(AuthContext);
 
     const handleLogOut = async () => {
         await logout();
@@ -54,6 +54,14 @@ export const UserAvatarDropDown: React.FC<IUserAvatar> = ({ avatar }) => {
                     className="z-10 absolute right-0 mt-2 w-44 bg-white divide-y divide-gray-100 rounded-lg dark:bg-gray-700 shadow-lg"
                 >
                     <ul className="py-2 text-sm text-gray-700 dark:text-gray-200">
+                        <li>
+                            <p className="block px-4 py-2 dark:hover:bg-gray-600 dark:hover:text-white">
+                                Balance:{" "}
+                                <span className="font-bold text-green-400">
+                                    {currentUser?.balance}
+                                </span>
+                            </p>
+                        </li>
                         <Link to={"/settings"}>
                             <li>
                                 <p className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
